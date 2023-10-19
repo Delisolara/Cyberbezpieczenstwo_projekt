04302cc0c1aa061d646883a0e685ec335b7ee076
Agnieszka Białecka <aga5467@gmail.com>
2023-10-19 22:14:17 +0200

    Dodanie listy użytkowników

---
+++ CyberBez_proj/Controllers/UserRolesController.cs
@@ -0,0 +1,37 @@
+﻿using CyberBez_proj.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CyberBez_proj.Controllers
+{
+    public class UserRolesController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        public UserRolesController(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var users = await _userManager.Users.ToListAsync();
+            var userRolesViewModel = new List<UserRolesViewModel>();
+            foreach (IdentityUser user in users)
+            {
+                var thisViewModel = new UserRolesViewModel();
+                thisViewModel.UserID = user.Id;
+                thisViewModel.UserName = user.UserName;
+                thisViewModel.Email = user.Email;
+                thisViewModel.Roles = await GetUserRoles(user);
+                userRolesViewModel.Add(thisViewModel);
+            }
+            return View(userRolesViewModel);
+        }
+        private async Task<List<string>> GetUserRoles(IdentityUser user)
+        {
+            return new List<string>(await _userManager.GetRolesAsync(user));
+        }
+    }
+}
