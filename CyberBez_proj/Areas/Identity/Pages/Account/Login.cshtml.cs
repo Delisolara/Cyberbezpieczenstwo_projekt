50a5c2499a668d3ab979938c971dc46e4340e680
Chmiel_Michał <92634372+chmielmichal@users.noreply.github.com>
2023-11-03 21:36:10 +0100

    Implementacja DNTCaptcha
    TODO: wyświetlanie się captchy pod okienkami do logowania, stworzenie logincontrollera?. Captcha umiejscowione w loginie z identity

--- CyberBez_proj/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ CyberBez_proj/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -14,6 +14,9 @@ using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using DNTCaptcha.Core;
+using Microsoft.Extensions.Options;
+
 
 namespace CyberBez_proj.Areas.Identity.Pages.Account
 {
@@ -21,11 +24,14 @@ namespace CyberBez_proj.Areas.Identity.Pages.Account
     {
         private readonly SignInManager<IdentityUser> _signInManager;
         private readonly ILogger<LoginModel> _logger;
-
-        public LoginModel(SignInManager<IdentityUser> signInManager, ILogger<LoginModel> logger)
+        private readonly IDNTCaptchaValidatorService _validatorService;
+        private readonly DNTCaptchaOptions _captchaOptions;
+        public LoginModel(SignInManager<IdentityUser> signInManager, ILogger<LoginModel> logger, IDNTCaptchaValidatorService validatorService, IOptions<DNTCaptchaOptions> captchaOptions)
         {
             _signInManager = signInManager;
             _logger = logger;
+            _validatorService = validatorService;
+            _captchaOptions = captchaOptions == null ? throw new ArgumentNullException(nameof(captchaOptions)) : captchaOptions.Value;
         }
 
         /// <summary>
@@ -60,26 +66,17 @@ namespace CyberBez_proj.Areas.Identity.Pages.Account
         /// </summary>
         public class InputModel
         {
-            /// <summary>
-            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-            ///     directly from your code. This API may change or be removed in future releases.
-            /// </summary>
+
             [Required]
             [EmailAddress]
             public string Email { get; set; }
 
-            /// <summary>
-            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-            ///     directly from your code. This API may change or be removed in future releases.
-            /// </summary>
+   
             [Required]
             [DataType(DataType.Password)]
             public string Password { get; set; }
 
-            /// <summary>
-            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-            ///     directly from your code. This API may change or be removed in future releases.
-            /// </summary>
+
             [Display(Name = "Remember me?")]
             public bool RememberMe { get; set; }
         }
@@ -100,7 +97,8 @@ namespace CyberBez_proj.Areas.Identity.Pages.Account
 
             ReturnUrl = returnUrl;
         }
-
+        [HttpPost]
+        [ValidateAntiForgeryToken]
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
         {
             returnUrl ??= Url.Content("~/");
@@ -109,9 +107,16 @@ namespace CyberBez_proj.Areas.Identity.Pages.Account
 
             if (ModelState.IsValid)
             {
+                if (!_validatorService.HasRequestValidCaptchaEntry(/*Language.English, DisplayMode.ShowDigits*/))
+                {
+                    ModelState.AddModelError(_captchaOptions.CaptchaComponent.CaptchaInputName, "Wprowadź kod.");
+                    return LocalRedirect(returnUrl);
+                }
+
                 // This doesn't count login failures towards account lockout
                 // To enable password failures to trigger account lockout, set lockoutOnFailure: true
                 var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: true);
+
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");
--- CyberBez_proj/Program.cs
+++ CyberBez_proj/Program.cs
@@ -1,144 +1,183 @@
 using CyberBez_proj.Data;
+using DNTCaptcha.Core;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using System.Configuration;
 
-
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddRoles<IdentityRole>()
-    .AddEntityFrameworkStores<ApplicationDbContext>();
-builder.Services.AddControllersWithViews();
-
-builder.Services.Configure<IdentityOptions>(options =>
-{
-    options.Password.RequireLowercase = false;
-    options.Password.RequireUppercase = false;
-    options.Password.RequiredLength = 12;
-    options.Password.RequireNonAlphanumeric = true;
-    options.Password.RequireDigit = true;
-
-    //Blokowanie u¿ytkowników na 15 min po wpisaniu Ÿle has³a 5 razy
-    options.Lockout.MaxFailedAccessAttempts = 5;
-    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(15);
-});
-
-builder.Services.Configure<PasswordHasherOptions>(option =>
-{
-    option.IterationCount = 12000;
-});
-
-builder.Services.ConfigureApplicationCookie(options => {
-    options.Cookie.Name = "AspNetCore.Identity.Application";
-    options.ExpireTimeSpan = TimeSpan.FromMinutes(15);
-    options.SlidingExpiration = true;
-});
-
-builder.Logging.ClearProviders();
-builder.Logging.AddConsole();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseMigrationsEndPoint();
-}
-else
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-app.MapRazorPages();
-
-using (var scope = app.Services.CreateScope())
-{
-    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
-
-    var roles = new[] { "Admin", "User" };
-
-    foreach (var role in roles)
+public class Program {
+    public static async Task Main(string[] args)
     {
-        if (!await roleManager.RoleExistsAsync(role))
-            await roleManager.CreateAsync(new IdentityRole(role));
-    }
-}
-using (var scope = app.Services.CreateScope())
-{
-    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
-    var roles = new string[] { "Administrator", "User" };
 
-    foreach (var role in roles)
-    {
-        if (!await roleManager.RoleExistsAsync(role))
-            await roleManager.CreateAsync(new IdentityRole(role));
-    }
-}
 
-using (var scope = app.Services.CreateScope())
-{
-    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+        var builder = WebApplication.CreateBuilder(args);
 
-    string mailAdmin = "admin@admin.admin";
-    string password = "a!A123456789";
-    string mailUser = "user@user.com";
+        // Add services to the container.
+        var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+        builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(connectionString));
+        builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-    if (await userManager.FindByEmailAsync(mailAdmin) == null)
-    {
-        var user = new IdentityUser
+        builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            .AddRoles<IdentityRole>()
+            .AddEntityFrameworkStores<ApplicationDbContext>();
+
+        builder.Services.AddControllersWithViews();
+        IWebHostEnvironment _env = builder.Environment;
+
+        builder.Services.AddDNTCaptcha(options =>
         {
-            UserName = mailAdmin,
-            Email = mailAdmin,
-            EmailConfirmed = true
-        };
+            options.UseCookieStorageProvider(SameSiteMode.Strict)
+            .AbsoluteExpiration(minutes: 7)
+            .ShowThousandsSeparators(false)
+            .WithEncryptionKey("This is my secure key!")
+            .InputNames(// This is optional. Change it if you don't like the default names.
+                new DNTCaptchaComponent
+                {
+                    CaptchaHiddenInputName = "DNT_CaptchaText",
+                    CaptchaHiddenTokenName = "DNT_CaptchaToken",
+                    CaptchaInputName = "DNT_CaptchaInputText"
+                })
+            .Identifier("dnt_Captcha")// This is optional. Change it if you don't like its default name.
+            ;
+        });
 
-        await userManager.CreateAsync(user, password);
-        await userManager.AddToRoleAsync(user, "Admin");
-    }
 
-    if (await userManager.FindByEmailAsync(mailUser) == null)
-    {
-        var user = new IdentityUser
+
+        builder.Services.Configure<IdentityOptions>(options =>
         {
-            UserName = mailUser,
-            Email = mailUser,
-            EmailConfirmed = true
-        };
+            options.Password.RequireLowercase = false;
+            options.Password.RequireUppercase = false;
+            options.Password.RequiredLength = 12;
+            options.Password.RequireNonAlphanumeric = true;
+            options.Password.RequireDigit = true;
 
-        await userManager.CreateAsync(user, password);
-        await userManager.AddToRoleAsync(user, "User");
-    }
+            //Blokowanie u¿ytkowników na 15 min po wpisaniu Ÿle has³a 5 razy
+            options.Lockout.MaxFailedAccessAttempts = 5;
+            options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(15);
+        });
 
-    for (int i = 0; i < 5; i++)
-    {
-        var user = new IdentityUser
+        //builder.Services
+        //.AddDNTCaptcha(option => option.UseCookieStorageProvider()
+        //.ShowThousandsSeparators(false)
+        //.WithEncryptionKey("123456"));
+
+      
+
+
+        // Other services and configurations
+
+        builder.Services.Configure<PasswordHasherOptions>(option =>
         {
-            UserName = $"user{i}@user.com",
-            Email = $"user{i}@user.com",
-            EmailConfirmed = true
-        };
-        await userManager.CreateAsync(user, password);
-        await userManager.AddToRoleAsync(user, "User");
+            option.IterationCount = 12000;
+        });
+
+        builder.Services.ConfigureApplicationCookie(options => {
+            options.Cookie.Name = "AspNetCore.Identity.Application";
+            options.ExpireTimeSpan = TimeSpan.FromMinutes(15);
+            options.SlidingExpiration = true;
+        });
+
+        builder.Logging.ClearProviders();
+        builder.Logging.AddConsole();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseMigrationsEndPoint();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Home/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}");
+        app.MapRazorPages();
+
+        using (var scope = app.Services.CreateScope())
+        {
+            var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+            var roles = new[] { "Admin", "User" };
+
+            foreach (var role in roles)
+            {
+                if (!await roleManager.RoleExistsAsync(role))
+                    await roleManager.CreateAsync(new IdentityRole(role));
+            }
+        }
+        using (var scope = app.Services.CreateScope())
+        {
+            var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var roles = new string[] { "Administrator", "User" };
+
+            foreach (var role in roles)
+            {
+                if (!await roleManager.RoleExistsAsync(role))
+                    await roleManager.CreateAsync(new IdentityRole(role));
+            }
+        }
+
+        using (var scope = app.Services.CreateScope())
+        {
+            var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+
+            string mailAdmin = "admin@admin.admin";
+            string password = "a!A123456789";
+            string mailUser = "user@user.com";
+
+            if (await userManager.FindByEmailAsync(mailAdmin) == null)
+            {
+                var user = new IdentityUser
+                {
+                    UserName = mailAdmin,
+                    Email = mailAdmin,
+                    EmailConfirmed = true
+                };
+
+                await userManager.CreateAsync(user, password);
+                await userManager.AddToRoleAsync(user, "Admin");
+            }
+
+            if (await userManager.FindByEmailAsync(mailUser) == null)
+            {
+                var user = new IdentityUser
+                {
+                    UserName = mailUser,
+                    Email = mailUser,
+                    EmailConfirmed = true
+                };
+
+                await userManager.CreateAsync(user, password);
+                await userManager.AddToRoleAsync(user, "User");
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                var user = new IdentityUser
+                {
+                    UserName = $"user{i}@user.com",
+                    Email = $"user{i}@user.com",
+                    EmailConfirmed = true
+                };
+                await userManager.CreateAsync(user, password);
+                await userManager.AddToRoleAsync(user, "User");
+            }
+
+        }
+
+        app.Run();
     }
-   
 }
-
-app.Run();
